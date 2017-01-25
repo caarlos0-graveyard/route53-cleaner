f8c49a16ba8f8a7b2103347a351366780d3f52ed
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-01-25 01:12:12 -0200

    added debug mode

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
@@ -13,11 +14,15 @@ import (
 )
 
 var version = "master"
+var debug bool
 
 func isUsed(record *route53.ResourceRecordSet, addrs []string) bool {
 	for _, r := range record.ResourceRecords {
 		for _, addr := range addrs {
 			if *r.Value == addr {
+				if debug {
+					log.Println("Used:", *r.Value, "=", addr)
+				}
 				return true
 			}
 		}
@@ -31,7 +36,18 @@ func main() {
 	app.Version = version
 	app.Author = "Carlos Alexandro Becker (root@carlosbecker.com)"
 	app.Usage = "Find records that could be deleted from your AWS Route53 hosted zones"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:        "debug, d",
+			Usage:       "show debug information",
+			Destination: &debug,
+		},
+	}
 	app.Action = func(c *cli.Context) error {
+		log.SetFlags(0)
+		if debug {
+			log.Println("Debug enabled!")
+		}
 		sess, err := session.NewSession()
 		if err != nil {
 			return cli.NewExitError(err.Error(), 1)
@@ -57,10 +73,17 @@ func main() {
 				addrs = append(addrs, *r.Value)
 			}
 		}
-
 		var removables []*route53.ResourceRecordSet
 		for _, record := range records {
+			if debug {
+				log.SetPrefix("")
+				log.Println("Checking", *record.Name)
+				log.SetPrefix("  --> ")
+			}
 			if *record.Type != "A" && *record.Type != "AAAA" && *record.Type != "CNAME" {
+				if debug {
+					log.Println("Type", *record.Type, "ignored")
+				}
 				continue
 			}
 			if !isUsed(record, addrs) {
@@ -69,8 +92,12 @@ func main() {
 		}
 		spin.Stop()
 
+		if debug {
+			log.SetPrefix("")
+			log.Println("\n")
+		}
 		for _, record := range removables {
-			fmt.Println(*record.Name, "might be removed")
+			log.Println(strings.TrimSuffix(*record.Name, "."))
 		}
 
 		return nil
